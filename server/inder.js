0846d109660cbcc481addf4acb1624fd0b450920
Deepak Singh Rajput <63790622+alkairis@users.noreply.github.com>
2021-07-01 23:36:45 +0530

    hosting done

--- server/inder.js
+++ server/inder.js
@@ -16,6 +16,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, resp) => {
+    resp.send('Welcome to alkairis')
+})
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
